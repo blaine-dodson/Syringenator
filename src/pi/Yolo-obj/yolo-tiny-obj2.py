af4a5a60acdf4c1683623f151cb92d73ce53c864
Alex Marlow <alexmarlow117@gmail.com>
2019-02-26 15:53:48 -0800

    added the tiny2 file and a new weights file, removed two old weights files

---
+++ src/pi/Yolo-obj/yolo-tiny-obj2.py
@@ -0,0 +1,120 @@
+# USAGE
+# python yolo.py --image images/baggage_claim.jpg --yolo yolo-coco
+# Original version from pyimagesource.com
+# edited by Alex Marlow
+
+# import the necessary packages
+import numpy as np
+import argparse
+import time
+
+import sys
+
+sys.path.append('/usr/local/lib/python3.5/dist-packages/cv2')
+
+import cv2
+import os
+netrez=800
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
+ap.add_argument("-y", "--yolo", type=str, default="yolo-coco",
+	help="base path to YOLO directory")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+ap.add_argument("-t", "--threshold", type=float, default=0.3,
+	help="threshold when applyong non-maxima suppression")
+args = vars(ap.parse_args())
+
+# load the syringe class labels our YOLO model was trained on
+labelsPath = os.path.sep.join([args["yolo"], "obj.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+
+# derive the paths to the YOLO weights and model configuration
+weightsPath = os.path.sep.join([args["yolo"], "yolov3-tiny-obj_16000_5.weights"])
+configPath = os.path.sep.join([args["yolo"], "yolov3-tiny-obj.cfg"])
+
+# load our YOLO object detector trained on syringe dataset (1 classes)
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+# load our input image and grab its spatial dimensions
+image = cv2.imread(args["image"])
+(H, W) = image.shape[:2]
+
+# determine only the *output* layer names that we need from YOLO
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# construct a blob from the input image and then perform a forward
+# pass of the YOLO object detector, giving us our bounding boxes and
+# associated probabilities
+blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (netrez,netrez),
+	swapRB=True, crop=False)
+net.setInput(blob)
+start = time.time()
+layerOutputs = net.forward(ln)
+end = time.time()
+
+# show timing information on YOLO
+print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+
+# initialize our lists of detected bounding boxes, confidences, and
+# class IDs, respectively
+boxes = []
+confidences = []
+classIDs = []
+
+# loop over each of the layer outputs
+for output in layerOutputs:
+	# loop over each of the detections
+	for detection in output:
+		# extract the class ID and confidence (i.e., probability) of
+		# the current object detection
+		scores = detection[5:]
+		classID = np.argmax(scores)
+		confidence = scores[classID]
+
+		# filter out weak predictions by ensuring the detected
+		# probability is greater than the minimum probability
+		if confidence > args["confidence"]:
+			# scale the bounding box coordinates back relative to the
+			# size of the image, keeping in mind that YOLO actually
+			# returns the center (x, y)-coordinates of the bounding
+			# box followed by the boxes' width and height
+			box = detection[0:4] * np.array([W, H, W, H])
+			(centerX, centerY, width, height) = box.astype("int")
+
+			#centerX  and centerY are the center quardinates
+
+			# use the center (x, y)-coordinates to derive the top and
+			# and left corner of the bounding box
+			x = int(centerX - (width / 2))
+			y = int(centerY - (height / 2))
+
+			# update our list of bounding box coordinates, confidences,
+			# and class IDs
+			boxes.append([x, y, int(width), int(height)])
+			confidences.append(float(confidence))
+			classIDs.append(classID)
+
+# apply non-maxima suppression to suppress weak, overlapping bounding
+# boxes
+idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
+	args["threshold"])
+
+# ensure at least one detection exists
+if len(idxs) > 0:
+	# loop over the indexes we are keeping
+	for i in idxs.flatten():
+		# extract the bounding box coordinates and print
+		(x, y) = (boxes[i][0], boxes[i][1])
+		(w, h) = (boxes[i][2], boxes[i][3])
+		print("x"+str(i)+":" + str(x) + " " + "y"+str(i)+":" + str(y))
+		print("w"+str(i)+":" + str(w) + " " + "h"+str(i)+":" + str(h))
+
+		text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+		print(str(i)+text)#added
+
