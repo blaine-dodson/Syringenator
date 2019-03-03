23c94f61a9c7d135aa50de26b73b1fd5e36c0d4b
Alex Boyle <alexboyle@D-10-16-14-227.dhcp4.washington.edu>
2019-03-02 17:33:47 -0800

    Added arm function, basic serial functionality, and line following. Need to add object avoidance next.

--- src/controller/MotorLib/MotorLib.h
+++ src/controller/MotorLib/MotorLib.h
@@ -8,39 +8,17 @@
 
 #include "Arduino.h"
 
-//The wheel assignments are from the perspective of the camera!
-//Left wheel pin assignments!
+extern volatile bool done_with_move;
 
-
-enum movementType{
-	forward,
-	backward,
-	left,
-	right,
-	pivotLeft,
-	pivotRight
-};
-
-struct movement {
-  movementType move; //look at movementType enum
-  byte duration; //How many rotations specifically
-  byte direction; // 1 = forward, 0 = backward
-};
+void motorSetup(byte l_motor_pwm = 13, byte l_motor_dir_a = 22, byte l_motor_dir_b = 24, byte r_motor_pwm = 10, byte r_motor_dir_a = 23, byte r_motor_dir_b = 25);
 
 void leftWheelInterrupt_ISR();
 void rightWheelInterrupt_ISR();
 
-void motorSetup(byte l_motor_pwm = 13, byte l_motor_dir_a = 22, byte l_motor_dir_b = 24, byte r_motor_pwm = 10, byte r_motor_dir_a = 23, byte r_motor_dir_b = 25);
-void robotGoTo(double x, double y);
-void moveRobot(movementType typeMove, double desired_rev, bool move_direction = 1);
-void intiateMove(movementType typeMove, double desired_rev, bool move_direction = 1, byte speedBoth = 255);
 void stop_motors();
-void moveStraight(bool direction, byte motorspeed = 255);
-double convertDistanceToRotations(double dist);
-//This method is used for a robot that is motionless and wants to turn sharply
-//This will be done by turning one wheel one direction and the other wheel will go the other
-void pivot(byte speed, bool direction);
-void rotate(double angle);
-void turn(byte Lspeed , byte Rspeed, bool direction);
+void moveFWBW(int ticks, byte motorspeed = 255);
+
+//This will do a conversion of the input angle to the arguments the pivot function needs
+void pivot(int ticks,byte speed =255);
 
 #endif
--- src/controller/controller.ino
+++ src/controller/controller.ino
@@ -1,20 +1,75 @@
 /**	@file controller.ino
- *
- *	The Arduino sketch.
- */
+ 	The Arduino sketch.
+*/
+#include <SensorLib.h>
+#include <MotorLib.h>
+#include <Syringenator.hpp>
+#include <Constants.hpp>
+#include <LobotServoController.h>
 
+char buffer[16];
+byte newCommand = 0;
 
-#include "Syringenator.hpp"
-
-/** Setup code here
- */
-void setup() {
-  
+void clearBuffer() {
+  for ( int i = 0; i < sizeof(buffer);  ++i )
+    buffer[i] = (char)0;
 }
 
-/** Controller loop code here
- */
+void setup() {
+  motorSetup();
+  setupSensor_ISR(); //use this to set up timer interrupts for line sensors and distance sensors
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  Serial.flush();
+  clearBuffer();
+}
+
 void loop() {
-  
-  
+  if (newCommand) {
+    parseCommand(buffer);
+    newCommand = 0;
+  }
+  delay(50);
+}
+
+void parseCommand(char input[ ]) { //commands sent to the arduino from the raspberry pi
+  switch (input[0]) {
+    case ARDUINO_ROTATE:
+      moveRotate(input[1]);
+      while (!isDoneCommand(1));
+      Serial.print(ARDUINO_STATUS_READY);
+      break;
+    case ARDUINO_MOVE:
+      moveStraight(input[1]);
+      while (!isDoneCommand(1));
+      Serial.print(ARDUINO_STATUS_READY);
+      break;
+    case ARDUINO_LINE_FOLLOW:
+      moveLineFollow();
+      Serial.print(ARDUINO_STATUS_READY);
+      break;
+    case 3:
+      stopLineFollow();
+      Serial.print(ARDUINO_STATUS_READY);
+      break;
+    case ARDUINO_ARM_PARK :
+    case ARDUINO_ARM_DISPOSE :
+    case ARDUINO_ARM_PICKUP:
+      grabObject(input[1], input[2], input[3]);
+      Serial.print(1);
+      break;
+
+  }
+  clearBuffer();
+}
+
+
+void serialEvent() {
+  int i = 0;
+  while (Serial.available()) {
+    // get the new byte:
+    buffer[i] = (char)Serial.read();
+    i++;
+  }
+  newCommand = 1;
 }
