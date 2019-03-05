bd798b53ef7073dcecad5d34fbc5988242b72e19
Brooke Stevenson <brooks04@uw.edu>
2019-03-04 21:56:00 -0800

    Changed the theta calculation for xArm (BS)

--- ArmTestSketch.ino/ArmTestSketch.ino.ino
+++ ArmTestSketch.ino/ArmTestSketch.ino.ino
@@ -2,9 +2,9 @@
 #include <SoftwareSerial.h>
 
 //Variable Declarations
-char angle = 90;
+char angle = 45;
 char radius = 19;
-char handAngle = 90;
+char handAngle = 0;
 uint16_t *servoPos;
 
 //#define rxPin 19
@@ -35,13 +35,9 @@ void grabObject(byte angle, byte radius, byte handAngle){
   //Calculate position based off the angle for ID:6
   //756 is a constant position for the ratio of 180 degrees to position number
   servos[5].ID = 6;
-  servos[5].Position = (angle/180.0)*1000;
+  servos[5].Position = (angle/180.0)*760 + 100;
   Serial.println(servos[5].Position);
-  //Calculate position based off the handAngle for ID:2
-  //881 is a constant position for the ratio of 90 degrees to position number
-  //90 being Position 500 (the default position)
   servos[1].ID = 2;
-  //servos[1].Position = 500 + (handAngle/90)*381;
   //All case statements are degrees with 90 being parallel to the robot
   switch(handAngle){
     case 45:
@@ -60,24 +56,6 @@ void grabObject(byte angle, byte radius, byte handAngle){
   //Set hand to default narrow open postion ID:1 which is 3cm wide
   servos[0].ID = 1;
   servos[0].Position = 500;
-  //Make sure hand is in position before moving to the radius and theta
-  //xArm.moveServo(1, 500, 1000);
-  //Calculate the position for the other 3 servos that are controlled by the radius
-  //ID:3-5
-  //This assumes that numbers given are based off of the center of the arm
-  //10cm is the reach of the xArm from the front of the robot
-  //Calculate (alpha) the angle of the base corners of the isoseles triangle
-//  //9.5cm is the length of arm between servos
-//  double alpha = acos(0.5*(radius/9.5));
-//  servos[2].ID = 3;
-//  servos[2].Position = (alpha/180.0)*1000 + 128;
-//  servos[3].ID = 4;
-//  if(radius == 19)
-//    servos[3].Position = 489;
-//  else
-//    servos[3].Position = ((180-(2*alpha))/180)*1000 + 489;
-//  servos[4].ID = 5;
-//  servos[4].Position = (alpha/180)*1000 + 103;
   //All case statements are in cm
   switch(radius){
     case 19:
@@ -160,11 +138,11 @@ void grabObject(byte angle, byte radius, byte handAngle){
   delay(2000);
   //Close the grip on the needle
   xArm.moveServo(1, 1000, 2000);
-  delay(2000);
+  delay(3000);
   //Drop the needle
   //Run action group 1, 1 time
   xArm.runActionGroup(1, 1);
-  delay(3000);
+  delay(4500);
   //Return to default resting position
   servos[0].Position = 498;
   servos[1].Position = 478;
